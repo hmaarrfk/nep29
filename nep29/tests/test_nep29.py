dd3b84afb754786bbb4817dffc6e7cef62cc6f52
Mark Harfouche <mark.harfouche@gmail.com>
2021-04-13 21:31:00 -0400

    Update test_nep29.py

--- nep29/tests/test_nep29.py
+++ nep29/tests/test_nep29.py
@@ -1,6 +1,13 @@
 import nep29
+from nep29 import nep29_versions
 import pytest
 
 
 def test_project_import():
     pass
+
+def test_pandas():
+    # Pre-release information can be included as letters
+    # not seperated by a '.' from the version number
+    # the pandas package uses this notation.
+    nep29_versions('pandas')
