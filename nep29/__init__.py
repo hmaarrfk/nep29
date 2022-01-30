699eb5f29eefb494f5c08e20db36037fbf768ae8
Mark Harfouche <mark.harfouche@gmail.com>
2022-01-30 13:07:04 -0500

    Use miniver

--- nep29/__init__.py
+++ nep29/__init__.py
@@ -4,7 +4,9 @@ __author__ = 'Mark Harfouche'
 __email__ = 'mark.harfouche@gmail.com'
 
 from .nep29 import nep29_versions
+from ._version import __version__
 
 __all__ = [
     'nep29_versions',
+    '__version__',
 ]
--- setup.py
+++ setup.py
@@ -2,6 +2,21 @@
 from setuptools import setup, find_packages
 
 
+def get_version_and_cmdclass(pkg_path):
+    """Load version.py module without importing the whole package.
+
+    Template code from miniver
+    """
+    import os
+    from importlib.util import module_from_spec, spec_from_file_location
+
+    version_py = os.path.join(pkg_path, "_version.py")
+    spec = spec_from_file_location("version", version_py)
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.__version__, module.get_cmdclass(pkg_path)
+
+
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
@@ -11,6 +26,8 @@ with open('HISTORY.md') as history_file:
 requirements = ['requests', 'prettytable', 'packaging', ]
 
 test_requirements = ['pytest', ]
+
+version, cmdclass = get_version_and_cmdclass(r"nep29")
 
 setup(
     author="Mark Harfouche",
@@ -43,5 +60,7 @@ setup(
             'nep29=nep29.nep29:main'
         ],
     },
+    version=version,
+    cmdclass=cmdclass,
     zip_safe=False,
 )
